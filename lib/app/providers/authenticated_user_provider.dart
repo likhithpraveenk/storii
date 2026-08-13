c4f3fba276d1660728820b965d692d8b091c60be
likhithpraveenk <lpkatlagunta@gmail.com>
2026-08-13 16:27:01 +0530

    fix: update user details

--- lib/app/providers/authenticated_user_provider.dart
+++ lib/app/providers/authenticated_user_provider.dart
@@ -3,6 +3,7 @@ import 'package:storii/app/models/user.dart';
 import 'package:storii/app/providers/api_providers.dart';
 import 'package:storii/app/providers/connection_providers.dart';
 import 'package:storii/app/providers/settings_provider.dart';
+import 'package:storii/features/auth/logic/users_provider.dart';
 import 'package:storii/shared/helpers/app_error.dart';
 import 'package:storii/shared/helpers/ref_extensions.dart';
 
@@ -17,13 +18,21 @@ Future<UserDomain> authenticatedUser(Ref ref) async {
   if (!isConnected) return user;
 
   try {
-    await ref.logApiCall(
-      // TODO: user details may have changed. update here
+    final response = await ref.logApiCall(
       () => ref.read(serverApiProvider(user)).authorize(),
       source: 'authenticatedUser',
       logMessage: 'Error authenticating user',
     );
-    return user;
+    final refreshed = UserDomain(
+      id: user.id,
+      username: response.user.username,
+      userType: response.user.type.name,
+      serverUrl: user.serverUrl,
+    );
+    if (refreshed != user) {
+      await ref.read(usersProvider.notifier).add(refreshed);
+    }
+    return refreshed;
   } on AppError catch (error) {
     if (error.type == .network || error.type == .timeout) {
       return user;
